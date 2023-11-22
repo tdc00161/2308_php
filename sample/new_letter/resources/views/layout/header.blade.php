f679f65f81b30fa889483af4a8d8524e8df2b936
tdc00161 <tdc00161@naver.com>
2023-11-23 01:58:32 +0900

    231123 new_letter login css작업

--- sample/new_letter/resources/views/layout/header.blade.php
+++ sample/new_letter/resources/views/layout/header.blade.php
@@ -1,4 +1,6 @@
 <header>
-    <h1>CK CK NEWSLETTER</h1>
-    <hr>
+    <p class="header_p">CK CK NEWSLETTER</p>
+    <input class="header_input" type="text">
+    <a class="header_btn" href="#">검색</a>
+    <a class="header_btn" href="#">로그인</a>
 </header>
--- sample/new_letter/resources/views/login.blade.php
+++ sample/new_letter/resources/views/login.blade.php
@@ -1,20 +1,29 @@
 @extends('layout.layout')
 
-@section('title','new_letter')
+@section('title','login')
 
 @section('main')
-<main class="login_main">
+<main>
+
     <form action="">
-        <div>
-            <label for="id">아이디</label>
-            <input type="text" id=id name=id>
-        </div>
-        <div>
-            <label for="pw">패스워드</label>
-            <input type="password" id=pw name=pw>
-        </div>
-        <button type="submit">로그인</button>
-        <a href="#">회원가입</a>
+
+        <div class="login_div">
+            <p class="login_title">CK CK NEWSLETTER</p>
+            <br>
+            <a class="login_bt" href="#">구글로 시작하기</a>
+            <br>
+            <hr class="login_hr">
+            <br>
+                <input type="text" id=id name=id placeholder="아이디">
+            <br>
+                <input type="password" id=pw name=pw placeholder="비밀번호">
+            <br>
+            <p class="login_pw">비밀번호를 잊으셨나요?</p>
+            <br><br>
+            <button class="login_btn" type="submit">로그인</button>
+            <br>
+            <a class="login_rg" href="#">회원가입하기</a>
+        </div>    
     </form>
 </main>
 @endsection
